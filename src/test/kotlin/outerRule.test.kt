ef0399a299b94caf7c0260c7d01389e437024884
scottmmjackson <scottmmjackson@gmail.com>
2016-07-08 16:51:56 -0400

    (test) added unit tests for two public-facing files

---
+++ src/test/kotlin/outerRule.test.kt
@@ -0,0 +1,17 @@
+/**
+ * Created by scottmmjackson on 7/8/16.
+ */
+
+package com.frankandrobot.rapier.test
+
+import org.jetbrains.spek.api.Spek
+
+import com.frankandrobot.rapier.outer
+
+class OuterRuleTest : Spek({
+  describe("#outer") {
+    it("Has no input or output.") {
+
+    }
+  }
+})
