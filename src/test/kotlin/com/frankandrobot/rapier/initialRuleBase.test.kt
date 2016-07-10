9af4eca4e60eac4bc19a866ccbe995460c6650f5
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-07-09 22:02:17 -0500

    [test] added nlp test

---
+++ src/test/kotlin/com/frankandrobot/rapier/initialRuleBase.test.kt
@@ -0,0 +1,22 @@
+package com.frankandrobot.rapier.test
+
+import org.jetbrains.spek.api.Spek
+
+
+class InitialRuleBaseTest : Spek({
+
+//  val text = """If you can't explain it simply, you don't understand it well enough."""
+//  val slot = Slot(name = "filler", value = )
+//
+//  describe("#initialRuleBase") {
+//    it("Should return rules when provided with Slot and Document") {
+//      val slot = Slot("Foo", "Bar")
+//      val document = Document("Foo foo foo foo");
+//      val initialRule = initialRuleBase(slot, document)
+//    }
+//  }
+//
+//  describe("#_initialRule") {
+//
+//  }
+})
---
+++ src/test/kotlin/com/frankandrobot/rapier/nlp/NlpProxy.test.kt
@@ -0,0 +1,37 @@
+package com.frankandrobot.rapier.test
+
+import com.frankandrobot.rapier.nlp.tokenize
+import org.jetbrains.spek.api.Spek
+import kotlin.test.assertEquals
+
+
+class NlpProxyTest : Spek({
+
+ // val text = """If you can't explain it simply, you don't understand it well enough."""
+
+  describe("#tokenize") {
+    it("should tokenize contractions and punctuations correctly") {
+
+      val text = "can't explain, you!"
+      var result = tokenize(text).map{ it.word }
+
+      assertEquals(listOf("ca", "n't", "explain", ",", "you", "!"), result)
+    }
+
+    it("should tokenize URLs correctly") {
+
+      val text = "http://foobar.com is good"
+      val result = tokenize(text).map{ it.word }
+
+      assertEquals(listOf("http://foobar.com", "is", "good"), result)
+    }
+
+    it("should tokenize hastags correctly") {
+
+      val text = "explain #simply"
+      val result = tokenize(text).map{ it.word }
+
+      assertEquals(listOf("explain", "#simply"), result)
+    }
+  }
+})
