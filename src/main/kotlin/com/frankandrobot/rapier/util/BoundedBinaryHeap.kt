a338bcff0b17384bbf91ea46f18703e8486e709b
Uriel Avalos <uriel.avalos@drillinginfo.com>
2016-07-14 00:24:27 -0500

    [heap] compiles

--- src/main/kotlin/com/frankandrobot/rapier/util/BoundedBinaryHeap.kt
+++ src/main/kotlin/com/frankandrobot/rapier/util/BoundedBinaryHeap.kt
@@ -3,13 +3,35 @@ package com.frankandrobot.rapier.util
 import java.util.*
 
 
-class BoundedBinaryHeap<T : Comparable<T>>(val size : Int = 0, internal val array : ArrayList<T> = ArrayList<T>(size)) {
+class BoundedBinaryHeap<T : Comparable<T>> protected constructor(size : Int, arrayFactory: (Int) -> Array<T?>) {
 
-  private var lastIndex = array.size - 1
+  internal val array = arrayFactory(size)
+
+  private var lastIndex = array.lastIndex
+
+
+  companion object {
+
+    inline fun <reified T : Comparable<T>> invoke(size: Int) =
+
+      BoundedBinaryHeap(size, { size -> arrayOfNulls<T>(size) })
+
+    internal inline fun <reified T : Comparable<T>> invoke(initialValues: ArrayList<T>, size: Int = initialValues.size) =
+
+      BoundedBinaryHeap(size, { size ->
+
+        val array = arrayOfNulls<T>(size)
+
+        initialValues.withIndex().forEach { array[it.index] = it.value }
+
+        array
+      })
+  }
+
 
   fun findMin() = array[1]
 
-  fun deleteMin() : T? {
+  fun deleteMin(): T? {
 
     if (lastIndex >= 0) {
 
@@ -26,25 +48,37 @@ class BoundedBinaryHeap<T : Comparable<T>>(val size : Int = 0, internal val arra
     return null
   }
 
-  internal fun percolateDown(i : Int, value : T) {
+  fun insert(value: T): Boolean {
 
-    val leftChildIndex = 2*i
-    val rightChildIndex = 2*i + 1
-    val leftChild = {array[leftChildIndex]}
-    val rightChild = {array[rightChildIndex]}
+    if (lastIndex < array.size - 1) {
+
+      percolateUp(value)
+
+      return true
+    }
+
+    return false
+  }
+
+  internal fun percolateDown(i: Int, value: T?) {
+
+    val leftChildIndex = 2 * i
+    val rightChildIndex = 2 * i + 1
+    val leftChild = { array[leftChildIndex] }
+    val rightChild = { array[rightChildIndex] }
+
 
     // both children exist
     if (rightChildIndex <= lastIndex) {
 
-      val smallerIndex = if (leftChild().compareTo(rightChild()) < 0) leftChildIndex else rightChildIndex
+      val smallerIndex = if (leftChild()!!.compareTo(rightChild()!!) < 0) leftChildIndex else rightChildIndex
       val smaller = array[smallerIndex]
 
-      if (smaller.compareTo(value) < 0) {
+      if (smaller!!.compareTo(value!!) < 0) {
 
         array[i] = smaller
         percolateDown(smallerIndex, value)
-      }
-      else {
+      } else {
 
         array[i] = value
       }
@@ -52,21 +86,41 @@ class BoundedBinaryHeap<T : Comparable<T>>(val size : Int = 0, internal val arra
     // only left child exists
     else if (leftChildIndex <= lastIndex) {
 
-      if (leftChild().compareTo(value) < 0) {
+      if (leftChild()!!.compareTo(value!!) < 0) {
 
         array[i] = leftChild()
         array[leftChildIndex] = value
-      }
-      else {
+      } else {
 
         array[i] = value
       }
     }
     // else is leaf node
-    else  {
+    else {
 
       array[i] = value
     }
   }
+
+  internal fun percolateUp(value: T) {
+
+    ++lastIndex
+
+    var currentIndex = lastIndex
+    var parentIndex = Math.floor(currentIndex / 2.0).toInt()
+    var parent = array[parentIndex]
+
+    while (value.compareTo(parent!!) < 0 && currentIndex >= 1) {
+
+      array[currentIndex] = parent
+
+      currentIndex = parentIndex
+      parentIndex = Math.floor(currentIndex / 2.0).toInt()
+      parent = array[parentIndex]
+    }
+
+    array[currentIndex] = value
+  }
 }
+
 
--- src/test/kotlin/com/frankandrobot/rapier/util/BoundedBinaryHeap.test.kt
+++ src/test/kotlin/com/frankandrobot/rapier/util/BoundedBinaryHeap.test.kt
@@ -9,19 +9,21 @@ class BoundedBinaryHeapTest : Spek({
 
   // val text = """If you can't explain it simply, you don't understand it well enough."""
 
-  describe("#deleteMin") {
-
-    var array = {arrayListOf(
+  val array = {
+    arrayListOf(
       0,
       2,
-      4, 3,
-      7, 5, 8, 10,
+      4,           3,
+      7,     5,    8,    10,
       11, 9, 6, 14
-    )}
+    )
+  }
+
+  describe("#deleteMin") {
 
     it("should return root") {
 
-      val heap = BoundedBinaryHeap(array = array())
+      val heap = BoundedBinaryHeap.invoke(initialValues = array())
 
       val result = heap.deleteMin()
 
@@ -30,7 +32,7 @@ class BoundedBinaryHeapTest : Spek({
 
     it("should percolate down") {
 
-      val heap = BoundedBinaryHeap(array = array())
+      val heap = BoundedBinaryHeap.invoke(initialValues = array())
       val afterDeletion = arrayListOf(
         0,
         3,
@@ -43,5 +45,81 @@ class BoundedBinaryHeapTest : Spek({
 
       assertEquals(afterDeletion, heap.array.dropLast(1))
     }
+  }
+
+  describe("#percolateUp") {
+
+    val simpleHeap = {arrayListOf(
+      0,
+      1,
+      2, 3
+    )}
+
+    it("should work on large values simple tree") {
+
+      val expected = arrayOf<Int?>(
+        0,
+        1,
+        2, 3,
+        4
+      )
+
+      val heap = BoundedBinaryHeap.invoke(initialValues = simpleHeap(), size = simpleHeap().size + 1)
+
+      heap.percolateUp(4)
+
+      assertEquals(expected, heap.array)
+    }
+
+    it("should work on small values simple tree") {
+
+      val expected = arrayOf<Int?>(
+        0,
+        0,
+        1, 3,
+        2
+      )
+
+      val heap = BoundedBinaryHeap.invoke(initialValues = simpleHeap(), size = simpleHeap().size + 1)
+
+      heap.percolateUp(0)
+
+      assertEquals(expected, heap.array)
+    }
+
+    it("should percolate up with large number") {
+
+      val heap = BoundedBinaryHeap.invoke(initialValues = array(), size = array().size + 1)
+      val afterPercolate = arrayOf<Int?>(
+        0,
+        2,
+        4,            3,
+        7,     5,     8, 10,
+        11, 9, 6, 14, 9
+
+      )
+
+      heap.percolateUp(9)
+
+      assertEquals(afterPercolate, heap.array)
+    }
+
+    it("should percolate up with tiny number") {
+
+      val heap = BoundedBinaryHeap.invoke(initialValues = array(), size = array().size + 1)
+      val afterPercolate = arrayOf<Int?>(
+        0,
+        1,
+        4,            2,
+        7,     5,     3, 10,
+        11, 9, 6, 14, 8
+
+      )
+
+      heap.percolateUp(1)
+
+      assertEquals(afterPercolate, heap.array)
+    }
+
   }
 })
