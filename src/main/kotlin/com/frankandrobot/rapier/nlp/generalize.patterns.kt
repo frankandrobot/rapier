67439ed0de4950c988613318d1d7e8f2e672b08f
Uriel Avalos <uriel.avalos@match.com>
2016-10-23 00:41:52 -0500

    [generalize] added tests for findExactMatches

--- src/main/kotlin/com/frankandrobot/rapier/nlp/generalize.patterns.kt
+++ src/main/kotlin/com/frankandrobot/rapier/nlp/generalize.patterns.kt
@@ -4,54 +4,76 @@ import com.frankandrobot.rapier.pattern.Pattern
 import java.util.*
 
 
-internal data class MatchIndeces(val leftIndex: Int, val rightIndex: Int)
+internal data class MatchIndices(val leftIndex: Int, val rightIndex: Int)
+
 
 internal fun sort(a : Pattern, b : Pattern) : Pair<Pattern, Pattern> {
 
-  val left = if (a().size <= b().size) a else b
-  val right = if (a().size <= b().size) b else a
+  val shorter = if (a().size <= b().size) a else b
+  val longer = if (a().size <= b().size) b else a
 
-  return Pair(left, right)
+  return Pair(shorter, longer)
 }
 
-internal fun findMatches(a : Pattern, b : Pattern) : ArrayList<MatchIndeces> {
+
+/**
+ * Find identical pattern elements in the two patterns subject to constraints:
+ *
+ * 1. matches must be in order i.e., in the patterns a:[x,y] and b:[y,x], pattern elements
+ *    x and y cannot both match.
+ * 2. every pattern element in the shorter pattern must be allowed to match one element
+ *    in the longer pattern.
+ */
+internal fun findExactMatchIndices(a : Pattern, b : Pattern) : ArrayList<MatchIndices> {
 
   val patterns = sort(a, b)
   val left = patterns.first
   val right = patterns.second
-
-  val matches = ArrayList<MatchIndeces>(left().size + 2)
+  val matches = arrayListOf(MatchIndices(-1, -1))
 
   left().withIndex().forEach { patternElement ->
 
-    val prevIndex : Int = if (matches.isNotEmpty()) matches[matches.lastIndex].rightIndex else -1
-
-    val searchRangeStart = Math.max(patternElement.index, prevIndex + 1)
+    val prevMatchIndex = matches[matches.lastIndex].rightIndex
+    val searchRangeStart = Math.max(patternElement.index, prevMatchIndex + 1)
     val searchRangeEnd = right().size - left().size + patternElement.index
 
     var i = searchRangeStart
 
     while (i <= searchRangeEnd) {
-
       if (patternElement.value == right()[i]) {
-
-        matches.add(MatchIndeces(patternElement.index, i))
+        matches.add(MatchIndices(patternElement.index, i))
+        break
       }
-
       ++i
     }
   }
 
-  return matches
+  return (matches + arrayListOf(MatchIndices(left().lastIndex+1, right().lastIndex+1)))
+    as ArrayList<MatchIndices>
 }
 
-fun findMatchingPatternSegments(a : Pattern, b : Pattern) : List<Pair<Pattern,Pattern>> {
+
+/**
+ * Splits the patterns by exact matches (if any). For example, given patterns
+ *
+ *     a:[x,y,z] and b:[1,x,2,3,z,4],
+ *
+ * it matches "x" and "z", so returns
+ *
+ *     [
+ *       Pair([], [1]),
+ *       Pair([x], [x]),
+ *       Pair([y], [2,3]),
+ *       Pair([z], [z]),
+ *       Pair([], [4])
+ *     ]
+ */
+internal fun partitionByExactMatches(a : Pattern, b : Pattern) : List<Pair<Pattern,Pattern>> {
 
   val patterns = sort(a, b)
   val left = patterns.first
   val right = patterns.second
-  val matches = arrayListOf(MatchIndeces(-1, -1)) +
-    findMatches(a, b) + arrayListOf(MatchIndeces(left().lastIndex, right().lastIndex))
+  val matches = findExactMatchIndices(a, b)
 
   if (matches.size > 2) {
 
--- src/test/kotlin/com/frankandrobot/rapier/nlp/GeneralizePatternsSpec.kt
+++ src/test/kotlin/com/frankandrobot/rapier/nlp/GeneralizePatternsSpec.kt
@@ -3,51 +3,92 @@ package com.frankandrobot.rapier.nlp
 import com.frankandrobot.rapier.pattern.Pattern
 import com.frankandrobot.rapier.pattern.PatternItem
 import org.jetbrains.spek.api.Spek
+import java.util.*
 import kotlin.test.assertEquals
 
 
 class GeneralizePatternsSpec : Spek ({
 
-  val anyPattern = Pattern(listOf("a", "b", "c").map{ PatternItem(it)})
 
-  describe("findMatches") {
+  describe("findExactMatchIndices") {
 
-    val anyLongerPattern = Pattern(listOf("1", "a", "3", "b", "c").map{ PatternItem(it)})
+    val anyShorterPattern = Pattern(listOf("a", "b", "c").map{ PatternItem(it) })
+    val anyLongerPattern = Pattern(listOf("1", "a", "3", "b", "c").map{ PatternItem(it) })
 
-    it ("should work") {
+    var result = ArrayList<MatchIndices>()
 
-      val result = findMatches(anyPattern, anyLongerPattern)
+    beforeEach{
+      result = findExactMatchIndices(anyShorterPattern, anyLongerPattern)
+    }
+
+    it("should return 3 matches plus 2 dummy fillers") {
+      assertEquals(5, result.size)
+    }
+
+    it("should return dummy filler at beginning") {
+      assertEquals(MatchIndices(leftIndex = -1, rightIndex = -1), result[0])
+    }
+
+    it("should return dummy filler at end") {
+      assertEquals(MatchIndices(leftIndex = 3, rightIndex = 5), result[4])
+    }
+
+    it("should match a, b, c") {
+      assertEquals(MatchIndices(leftIndex = 0, rightIndex = 1), result[1])
+      assertEquals(MatchIndices(leftIndex = 1, rightIndex = 3), result[2])
+      assertEquals(MatchIndices(leftIndex = 2, rightIndex = 4), result[3])
+    }
+
+    it("should return the first match") {
+      val left = Pattern(listOf("1", "x", "2").map{ PatternItem(it) })
+      val right = Pattern(listOf("3", "x", "x").map{ PatternItem(it) })
+      val result = findExactMatchIndices(left, right)
+
+      assertEquals(MatchIndices(leftIndex = 1, rightIndex = 1), result[1])
+      assertEquals(3, result.size)
+    }
+
+    it("should NOT return match if elements in shorter pattern cannot match up 1") {
+      val left = Pattern(listOf("1", "x", "y").map{ PatternItem(it) })
+      val right = Pattern(listOf("3", "4", "5", "x").map{ PatternItem(it) })
+      val result = findExactMatchIndices(left, right)
+
+      assertEquals(2, result.size)
+    }
+
+    it("should NOT return match if elements in shorter pattern cannot match up 2") {
+      val left = Pattern(listOf("1", "x", "y").map{ PatternItem(it) })
+      val right = Pattern(listOf("3", "4", "5", "y", "x").map{ PatternItem(it) })
+      val result = findExactMatchIndices(left, right)
 
       assertEquals(3, result.size)
-      assertEquals(MatchIndeces(leftIndex = 0, rightIndex = 1), result[0])
-      assertEquals(MatchIndeces(leftIndex = 1, rightIndex = 3), result[1])
-      assertEquals(MatchIndeces(leftIndex = 2, rightIndex = 4), result[2])
+      assertEquals(MatchIndices(leftIndex = 2, rightIndex = 3), result[1])
     }
   }
 
-  describe("caseEqualSize") {
-
-    it ("should work") {
-
-      val anotherSameSizePattern = Pattern(listOf("1", "2", "3").map{ PatternItem(it)})
-
-      val result = caseEqualSize(anyPattern, anotherSameSizePattern)
-
-      // should contain all combinations of:
-      // [], [a,1]
-      // [], [b,2]
-      // [], [c,3]
-      // so a total of 8 patterns
-      assertEquals(8, result.size)
-      //empty
-      assert(result.contains(Pattern(PatternItem(), PatternItem(), PatternItem())))
-      assert(result.contains(Pattern(PatternItem(), PatternItem(), PatternItem("c", "3"))))
-      assert(result.contains(Pattern(PatternItem(), PatternItem("b", "2"), PatternItem())))
-      assert(result.contains(Pattern(PatternItem(), PatternItem("b", "2"), PatternItem("c", "3"))))
-      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem(), PatternItem())))
-      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem(), PatternItem("c", "3"))))
-      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem("b", "2"), PatternItem())))
-      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem("b", "2"), PatternItem("c", "3"))))
-    }
-  }
+//  describe("caseEqualSize") {
+//
+//    it ("should work") {
+//
+//      val anotherSameSizePattern = Pattern(listOf("1", "2", "3").map{ PatternItem(it)})
+//
+//      val result = caseEqualSize(anyPattern, anotherSameSizePattern)
+//
+//      // should contain all combinations of:
+//      // [], [a,1]
+//      // [], [b,2]
+//      // [], [c,3]
+//      // so a total of 8 patterns
+//      assertEquals(8, result.size)
+//      //empty
+//      assert(result.contains(Pattern(PatternItem(), PatternItem(), PatternItem())))
+//      assert(result.contains(Pattern(PatternItem(), PatternItem(), PatternItem("c", "3"))))
+//      assert(result.contains(Pattern(PatternItem(), PatternItem("b", "2"), PatternItem())))
+//      assert(result.contains(Pattern(PatternItem(), PatternItem("b", "2"), PatternItem("c", "3"))))
+//      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem(), PatternItem())))
+//      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem(), PatternItem("c", "3"))))
+//      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem("b", "2"), PatternItem())))
+//      assert(result.contains(Pattern(PatternItem("a", "1"), PatternItem("b", "2"), PatternItem("c", "3"))))
+//    }
+//  }
 })
