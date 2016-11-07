048cc41a0645cf9219c6990c669d50d6089666d1
Uriel Avalos <uriel.avalos@match.com>
2016-11-06 20:38:30 -0600

    [test] added missing pattern element equality check

--- src/test/kotlin/com/frankandrobot/rapier/pattern/PatternElementSpec.kt
+++ src/test/kotlin/com/frankandrobot/rapier/pattern/PatternElementSpec.kt
@@ -13,16 +13,44 @@ class PatternElementSpec : Spek({
 
     describe("PatternItem") {
 
-      it("should work on empty constraints") {
-
+      it("should work on unconstrained patterns") {
         val elem = PatternItem()
-
         assertEquals(true, elem.test(anyToken))
       }
     }
 
     describe("PatternList") {
 
+    }
+
+    describe("PatternItem equality") {
+      it("should work when equal constrains are out of order") {
+        val a = PatternItem(
+          listOf("a", "b").map{WordConstraint(it)}.toHashSet(),
+          listOf("tag1", "tag2").map{PosTagConstraint(it)}.toHashSet()
+        )
+        val b = PatternItem(
+          listOf("b", "a").map{WordConstraint(it)}.toHashSet(),
+          listOf("tag2", "tag1").map{PosTagConstraint(it)}.toHashSet()
+        )
+        assertEquals(true, a == b)
+      }
+    }
+
+    describe("PatternList equality") {
+      it("should work when equal constrains are out of order") {
+        val a = PatternList(
+          listOf("a", "b").map{WordConstraint(it)}.toHashSet(),
+          listOf("tag1", "tag2").map{PosTagConstraint(it)}.toHashSet(),
+          length = 1
+        )
+        val b = PatternList(
+          listOf("b", "a").map{WordConstraint(it)}.toHashSet(),
+          listOf("tag2", "tag1").map{PosTagConstraint(it)}.toHashSet(),
+          length = 1
+        )
+        assertEquals(true, a == b)
+      }
     }
   }
 })
