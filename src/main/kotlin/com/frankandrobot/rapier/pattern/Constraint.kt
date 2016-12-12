2b3fefbca76a919d2d63718a0589499e5f415b4f
Uriel Avalos <uriel.avalos@match.com>
2016-12-12 00:34:42 -0600

    [refac] constraint rewritten to use Option

--- src/main/kotlin/com/frankandrobot/rapier/pattern/Constraint.kt
+++ src/main/kotlin/com/frankandrobot/rapier/pattern/Constraint.kt
@@ -1,37 +1,52 @@
 package com.frankandrobot.rapier.pattern
 
 import com.frankandrobot.rapier.nlp.Token
+import org.funktionale.option.Option
 
 
 interface Constraint {
 
-  val value : String
+  val value : Option<String>
 
   fun satisfies(token: Token) : Boolean
 }
 
 
-data class PosTagConstraint(override val value: String = "") : Constraint {
+data class PosTagConstraint(override val value : Option<String> = Option.None) :
+  Constraint {
+
+  internal constructor(value : String) : this(Option.Some(value)) {}
 
   override fun satisfies(token: Token) = token.posTag.equals(value)
 
-  override fun toString() = value
+  override fun toString() = value.toString()
 }
 
 
-data class WordConstraint(override val value: String = "") : Constraint {
+data class WordConstraint(override val value : Option<String> = Option.None) :
+  Constraint {
+
+  internal constructor(value : String) : this(Option.Some(value)) {}
 
   override fun satisfies(token: Token) = token.word.equals(value)
 
-  override fun toString() = value
+  override fun toString() = value.toString()
 }
 
 
-data class SemanticConstraint(override val value: String = "") : Constraint {
+data class SemanticConstraint(override val value : Option<String> = Option.None) :
+  Constraint {
+
+  internal constructor(value : String) : this(Option.Some(value)) {}
 
   override fun satisfies(token: Token) = token.semanticClass.equals(value)
 
-  override fun toString() = value
+  override fun toString() = value.toString()
 }
 
 
+fun words(vararg words : String) =
+  words.map { WordConstraint(Option.Some(it)) }.toHashSet()
+
+fun tags(vararg tags : String) =
+  tags.map{ PosTagConstraint(Option.Some(it)) }.toHashSet()
