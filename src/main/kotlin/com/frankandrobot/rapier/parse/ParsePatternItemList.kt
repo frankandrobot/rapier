e8e1a228e8d025d0bbf63b3acd0dbb5bebb77765
Uriel Avalos <uriel.avalos@match.com>
2016-12-13 01:51:02 -0600

    [parse] PatternExpandedForm passes tests again

--- src/main/kotlin/com/frankandrobot/rapier/parse/ParsePatternItemList.kt
+++ src/main/kotlin/com/frankandrobot/rapier/parse/ParsePatternItemList.kt
@@ -1,23 +1,23 @@
 package com.frankandrobot.rapier.parse
 
 import com.frankandrobot.rapier.pattern.PatternItem
-import com.frankandrobot.rapier.pattern.WordConstraint
 import com.frankandrobot.rapier.pattern.words
 import java.util.*
 
 
 /**
- * Note how this class is not a PatternElement.
- *
- * It's for parsing, not Rule creation. It exists because Kotlin isn't dynamically typed.
+ * Note how this class is not a PatternElement. It's just a list of PatternItems.
+ * It is used by the PatternList class. One PatternList expands into one or more
+ * ParsePatternItemLists.
  */
-data class ParsePatternItemList(val items: ArrayList<PatternItem> = ArrayList<PatternItem>()) {
+data class ParsePatternItemList(
+  val items : ArrayList<PatternItem> = ArrayList<PatternItem>()) {
 
   internal constructor(vararg patternItem : PatternItem)
   : this((ArrayList<PatternItem>() + patternItem.asList()) as ArrayList<PatternItem>)
 
-  internal constructor(vararg constraints : WordConstraint)
-  : this((ArrayList<PatternItem>() + constraints.map{PatternItem(it)}) as ArrayList<PatternItem>)
+//  internal constructor(vararg constraints : WordConstraint)
+//  : this((ArrayList<PatternItem>() + constraints.map{PatternItem(it)}) as ArrayList<PatternItem>)
 
   internal constructor(vararg word : String)
   : this((ArrayList<PatternItem>() + word.map{PatternItem(words(it))}) as ArrayList<PatternItem>)
