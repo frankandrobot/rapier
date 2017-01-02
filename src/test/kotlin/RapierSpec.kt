836241210e235fc38945aae39c3c5012530c346d
Uriel Avalos <uriel.avalos@match.com>
2017-01-02 09:53:03 -0600

    [rest] rapier tests added

--- src/test/kotlin/RapierSpec.kt
+++ src/test/kotlin/RapierSpec.kt
@@ -20,14 +20,14 @@ class RapierSpec : Spek({
       )
       val example = Example(
         blankTemplate,
-        document,
+        roleDocument,
         FilledTemplate(slots(
           SlotName("role") to slotFillers(wordTokens("Senior", "Software", "Engineer"))
         ))
       )
       val example2 = Example(
         blankTemplate,
-        document2,
+        roleDocument2,
         FilledTemplate(slots(
           SlotName("role") to slotFillers(wordTokens("Senior", "Software", "Engineer"))
         ))
@@ -41,7 +41,7 @@ class RapierSpec : Spek({
         ruleSizeWeight = 100.0
       )
 
-      it("should find a rule with empty pre/post fillers") {
+      it("should find a rule") {
         val result = rapier(blankTemplate, examples = examples, params = params)
         result[0].learnedRules.map(::toBaseRule) shouldEqual listOf(
           BaseRule(
@@ -61,10 +61,62 @@ class RapierSpec : Spek({
         )
       }
     }
+
+    describe("two examples with similar fillers") {
+
+      val blankTemplate = BlankTemplate(
+        name = "test",
+        slots = slotNames("exp")
+      )
+      val example = Example(
+        blankTemplate,
+        yearsDocument1,
+        FilledTemplate(slots(
+          SlotName("exp") to slotFillers(wordTokens("5", "+", "years", "experience"))
+        ))
+      )
+      val example2 = Example(
+        blankTemplate,
+        yearsDocument2,
+        FilledTemplate(slots(
+          SlotName("exp") to slotFillers(wordTokens("6", "+", "years"))
+        ))
+      )
+      val examples = Examples(listOf(example, example2))
+      val params = RapierParams(
+        Random = Random(20),
+        compressionFails = 7,
+        metricMinPositiveMatches = 1,
+        compressionPriorityQueueSize = 5
+        //ruleSizeWeight = 0.0
+      )
+
+      it("should find a rule") {
+        val result = rapier(blankTemplate, examples = examples, params = params)
+        print(result[0].learnedRules.map(::toBaseRule))
+        result[0].learnedRules.map(::toBaseRule) shouldEqual listOf(
+          BaseRule(
+            preFiller = Pattern(
+              PatternList(words("*"), tags("SYM"), length = 1)
+            ),
+            filler = Pattern(
+              PatternItem(posTagConstraints = tags("CD")),
+              PatternItem(words("+"), tags("SYM")),
+              PatternItem(words("years"), tags("NNS")),
+              PatternList(words("experience"),tags("NN"), length = 1)
+            ),
+            postFiller = Pattern(
+              PatternItem(posTagConstraints = tags("IN", "VBG"))
+            ),
+            slotName=SlotName(name="exp")
+          )
+        )
+      }
+    }
   }
 })
 
-private val document = Document(Some("""
+private val roleDocument = Document(Some("""
 As a Senior Software Engineer in our Engineering team you will lead the technical
 efforts to expand Cognitive Scale’s product capabilities.  You will work closely with
 the CTO and the advanced technology team to create unique and differentiating
@@ -78,7 +130,7 @@ have the opportunity to put cognitive computin g in action to truly change the w
 Come join us on our journey....
     """))
 
-private var document2 = Document(Some("""
+private var roleDocument2 = Document(Some("""
 The role is a self-organized Senior Software Engineer with solid Java and web service
 development experience.  In this position, you are first and foremost a passionate and
 talented developer that can work in a dynamic environment as a member of Agile Scrum
@@ -97,3 +149,34 @@ Implementation, you will create mobile-friendly mirrors of clients’ career sit
 make sure our software knows how to send jobseekers’ applications correctly to the
 original site.
 """)))
+
+private var yearsDocument1 = Document(Some("""
+Requirements:
+
+* 5+ years experience in Object Oriented Design and Programming or Functional Programing
+* Hands-on expertise with Java, JavaScript, Python, or Scala
+* Experience working on large software systems, particularly distributed systems and
+microservices architectures
+* Experience architecting large software systems and enterprise integrations
+* Solid grasp and fluent in common design patterns
+* Experience working with large transactional data streams and combining transactional
+data with web, social and mobile data streams
+* Demonstrated ability to effectively establish and maintain working relationships with
+all levels of the organization
+* Keen business judgment, focus, and ability to see the "big picture" and prioritize
+* Excellent communication and presentation skills with the ability to present and
+translate complex information to both internal and external teams in relevant business terms
+"""))
+
+private var yearsDocument2 = Document(Some("""
+We are looking for someone with:
+* Bachelor's degree in Computer Science or similar desired
+* 6+ years developing high-quality software
+* Experience with .NET, Node.js, Java
+* Strong knowledge of services (mostly RESTful)
+* AWS experience
+* Database knowledge; SQLServer and MongoDB
+* Strong interest in learning and expanding your personal toolbox of skills
+* High emotional intelligence
+"""))
+
