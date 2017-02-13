daaa30402f202d1e1f2edca55c269fe37367738b
Uriel Avalos <uriel.avalos@match.com>
2017-02-12 22:03:21 -0600

    [jwi] getting iterator per POS

--- src/main/kotlin/com/frankandrobot/rapier/nlp/jwi/SemanticClassIterator.kt
+++ src/main/kotlin/com/frankandrobot/rapier/nlp/jwi/SemanticClassIterator.kt
@@ -5,25 +5,25 @@ import edu.mit.jwi.item.ISynset
 import edu.mit.jwi.item.POS
 import edu.mit.jwi.item.Pointer
 import org.funktionale.option.Option
-import org.funktionale.option.Option.*
+import org.funktionale.option.Option.None
+import org.funktionale.option.Option.Some
 import java.util.*
 
-
-private val posList = listOf(POS.ADJECTIVE, POS.ADVERB, POS.NOUN, POS.VERB)
 
 /**
  * Iterates thru the semantic class heiarachy one level at a time i.e.,
  * #next returns the next level of semantic classes.
  */
-data class SemanticClassIterator(val dict : IDictionary, val word : String) :
-  Iterator<HashSet<ISynset>> {
+data class SemanticClassIterator(val dict : IDictionary,
+                                 val lemma: String,
+                                 val pos : POS) : Iterator<HashSet<ISynset>> {
 
   private var calculatedNext = false
   private var next : Option<HashSet<ISynset>> = None
 
   private fun initialSemanticClasses() : HashSet<ISynset> {
-    val idxWords = posList
-      .map{dict.getIndexWord(word, it)}
+    val idxWords = listOf(1)
+      .map{dict.getIndexWord(lemma, pos)}
       .filter{it != null}
       .map{it!!}
     val synsets = idxWords
